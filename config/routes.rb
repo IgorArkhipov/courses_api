838e79f10a9d8e5cecaa6a0b31804f18e477a989
Igor Arkhipov <igor.arkhipov@crossengage.io>
2023-01-07 13:20:44 +0100

    Initial commit

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  # Api definition
+  namespace :api, defaults: { format: :json } do
+    # We are going to list our resources here
+  end
+end
